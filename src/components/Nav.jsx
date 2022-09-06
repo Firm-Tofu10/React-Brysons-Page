2cfd82b0cc41f7443aac56196eb8f91a9686fa09
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-06 15:55:21 -0700

    Update Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav (props) {
+	console.log("Nav Props", props)
+	return (
+		<nav className='Navbar'>
+		<div className='navbar-box'>
+		<link to='/' className='navbar-picture'>
+
+		</link>
+		</div> 
+		</nav>
+	
+	)
+
+}
+export default Nav
