3c0e71fe088727b1d146a72bfca599e97004ed77
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-09 18:30:29 -0700

    asdasd
    asdasd

--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 Header Photo link:https://unsplash.com/photos/LAaSoL0LrYs
+Code to help create containers learned here:https://www.geeksforgeeks.org/how-to-use-container-component-in-reactjs/
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import './App.css'
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
@@ -10,6 +12,34 @@ function App() {
 			<Nav nav='About Me-Portfolio-Resume-Contact' />
 			<Footer version='V0.1.1----' />
 			
+			<Container id='about' maxWidth="sm">
+      <Typography component="div" style={{ 
+        backgroundColor: 'White', height: '30vh', width: '50vh', position: 'flex',marginbottom: '2rem',justifyContent: 'center', alignItems: 'center'
+      }}>
+        About me!
+      </Typography>
+    </Container>
+		<Container id='portfolio' maxWidth="sm">
+      <Typography component="div" style={{ 
+        backgroundColor: 'Wheat', height: '30vh', width: '50vh'
+      }}>
+        Portfolio
+      </Typography>
+    </Container>
+		<Container id='resume' maxWidth="sm">
+      <Typography component="div" style={{ 
+        backgroundColor: 'Grey', height: '30vh', width: '50vh' 
+      }}>
+        Resume
+      </Typography>
+    </Container>
+		<Container id='contact' maxWidth="sm">
+      <Typography component="div" style={{ 
+        backgroundColor: 'Green', height: '30vh', width: '50vh'
+      }}>
+        Contact
+      </Typography>
+    </Container>
     </div>
   );
 }
